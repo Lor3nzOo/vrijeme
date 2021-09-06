1665f365cffb326de26b4a216187661684b08c74
Lor3nzOo <lorenzomelon68@gmail.com>
2021-09-06 21:41:18 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -2,128 +2,110 @@ import React, { useState } from "react";
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
-  const [city, setCity] = useState("");
-  const [state, setState] = useState("");
-  const [zip, setZip] = useState("");
-
-    const [weatherData7, setWeatherData7] = useState(null);
-
+  const [city, setCity] = useState(null);
+  const [state, setState] = useState(null);
+  const [zip, setZip] = useState(null);
 
   function handleCity(e) {
     setCity(e.target.value.toLowerCase());
   }
 
-  function handleState(e){
-      setState(e.target.value.toLowerCase());
+  function handleState(e) {
+    setState(e.target.value.toLowerCase());
   }
 
-    function handleZip(e){
-        setZip(e.target.value.toLowerCase());
-    }
+  function handleZip(e) {
+    setZip(e.target.value.toLowerCase());
+  }
 
   function getWeatherData() {
     const navL = navigator.language;
-    const nav = navL.split('-');
+    const nav = navL.split("-");
 
     fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${zip}&appid=909b25942a91dd2c10a0a46a8e77451a&lang=${nav[0]}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${zip}&appid=909b25942a91dd2c10a0a46a8e77451a&lang=${nav[0]}&units=metric`
     )
-        .then((response) => response.json())
-        .then((data) => {
-          setWeatherData(data);
-          console.log(data);
-        })
-        .catch(() => {
-          console.log("error");
-        });
-  }
-
-  function getWeather7(){
-      navigator.geolocation.getCurrentPosition(position => {
-
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-
-      const navL = navigator.language;
-      const nav = navL.split('-');
-
-      fetch(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=909b25942a91dd2c10a0a46a8e77451a&lang=${nav[0]}&units=metric`
-      )
-          .then((response) => response.json())
-          .then((data7) => {
-              setWeatherData7(data7);
-              console.log(data7);
-          })
-          .catch(() => {
-              console.log("error");
-          });
+      .then(response => response.json())
+      .then(data => {
+        setWeatherData(data);
+      })
+      .catch(error => {
+        console.log(error);
       });
   }
 
-  if (weatherData !== null){
-      const icon = weatherData.weather.map(({ icon }) => icon);
-      const src = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
-      return (
-          <div className="App">
-              <section className="controls">
-                  <input
-                      type="text"
-                      placeholder="City"
-                      onChange={handleCity}
-                  />
-                  <input
-                      type="text"
-                      placeholder="Country"
-                      onChange={handleState}
-                  />
-                  <input
-                      type="text"
-                      placeholder="Zip code"
-                      onChange={handleZip}
-                  />
-                  <button onClick={() => { getWeatherData(); getWeather7();}}>Get Weather</button>
-                  <ul>
-                      <li className="lista">Weather: {weatherData.weather.map(({ description }) => description.charAt(0).toUpperCase() + description.slice(1))}</li>
-                      <li className="lista2"><img alt="picture" src={src} /></li>
-                      <li className="lista2">Temperature: {weatherData.main.temp}&deg;C</li>
-                      <li className="lista">Feels like: {weatherData.main.feels_like}&deg;C</li>
-                      <li className="lista">Minimal temperature: {weatherData.main.temp_min}&deg;C</li>
-                      <li className="lista">Max temperature: {weatherData.main.temp_max}&deg;C</li>
-                      <li className="lista">Humidity: {weatherData.main.humidity}%</li>
-                      <li className="lista">Pressure: {weatherData.main.pressure}hPa</li>
-                      <li className="lista">Clouds: {weatherData.clouds.all}%</li>
-                      <li className="lista">Wind speed: {weatherData.wind.speed.toFixed(1) * 3.60.toFixed(0)}km/h</li>
-                  </ul>
-              </section>
+  if (weatherData !== null) {
+    const icon = weatherData.weather.map(({ icon }) => icon);
+    const src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-          </div>
-      )
+    return (
+      <div className="App">
+        <section className="controls">
+          <input type="text" placeholder="Grad" onChange={handleCity} />
+          <input type="text" placeholder="Država" onChange={handleState} />
+          <input
+            type="text"
+            placeholder="Poštanski broj"
+            onChange={handleZip}
+          />
+          <button onClick={getWeatherData}>Klikni za vrijeme</button>
+          <ul>
+            <li className="lista">
+              Vrijeme:{" "}
+              {weatherData.weather.map(
+                ({ description }) =>
+                  description.charAt(0).toUpperCase() + description.slice(1)
+              )}
+            </li>
+            <li className="lista2">
+              <img alt="picture" src={src} />
+            </li>
+            <li className="lista2">
+              Temperatura: {weatherData.main.temp}&deg;C
+            </li>
+            <li className="lista">
+              Osjet: {weatherData.main.feels_like}&deg;C
+            </li>
+            <li className="lista">
+              Minimalna temperatura: {weatherData.main.temp_min}&deg;C
+            </li>
+            <li className="lista">
+              Maksimalna temperatura: {weatherData.main.temp_max}&deg;C
+            </li>
+            <li className="lista">Vlažnost: {weatherData.main.humidity}%</li>
+            <li className="lista">Tlak: {weatherData.main.pressure}hPa</li>
+            <li className="lista">Oblaci: {weatherData.clouds.all}%</li>
+            <li className="lista">
+              Brzina vjetra:{" "}
+              {weatherData.wind.speed.toFixed(1) * (3.6).toFixed(0)}
+              km/h
+            </li>
+          </ul>
+        </section>
+        <div className="section3">
+          <h1>&copy; Lorenzo Melon 2021</h1>
+        </div>
+      </div>
+    );
   } else {
-      return (
-          <div className="App">
-              <section className="controls">
-                  <input
-                      type="text"
-                      placeholder="City"
-                      onChange={handleCity}
-                  />
-                  <input
-                      type="text"
-                      placeholder="Country"
-                      onChange={handleState}
-                  />
-                  <input
-                      type="text"
-                      placeholder="Zip code"
-                      onChange={handleZip}
-                  />
-                  <button onClick={() => { getWeatherData(); getWeather7();}}>Get Weather</button>
-              </section>
-          </div>
-      )
+    return (
+      <div className="App">
+        <section className="controls">
+          <input type="text" placeholder="Grad" onChange={handleCity} />
+          <input type="text" placeholder="Država" onChange={handleState} />
+          <input
+            type="text"
+            placeholder="Poštanski broj"
+            onChange={handleZip}
+          />
+          <button onClick={getWeatherData}>Klikni za vrijeme</button>
+        </section>
+        <div className="section2">
+          <h1>&copy; Lorenzo Melon 2021</h1>
+        </div>
+      </div>
+    );
   }
-
 }
 
 export default App;
